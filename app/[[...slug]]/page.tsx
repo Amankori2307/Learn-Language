b8831e9e3e56163a2db04ca4f0af4b7b5abcf43c
Tech Anarix <tech@anarix.ai>
2026-02-24 17:28:09 +0530

    [TASK P8-007] enable SSR route rendering path for Next catch-all app

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -1,5 +1,7 @@
 import NextSpaRoot from "../../client/src/next-spa-root";
 
+export const dynamic = "force-dynamic";
+
 export default function SpaPage() {
   return <NextSpaRoot />;
 }
--- client/src/next-spa-root.tsx
+++ client/src/next-spa-root.tsx
@@ -1,18 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import App from "./App";
 
 export default function NextSpaRoot() {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
-
   return <App />;
 }
